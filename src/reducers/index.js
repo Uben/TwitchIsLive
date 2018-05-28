2e078b39f26aba3103e7d645a43c521f07a3074e
Bernin Alejandro Uben <berninuben@icloud.com>
2018-05-28 15:32:19 -0400

    added redux-thunk to handle async ajax requests w/ axios

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import ChannelSearch from './reducer_channel_search.js';
-import InputChange from './reducer_handle_input_change.js';
+import ChannelSearch from './reducer_channel_search';
+import InputChange from './reducer_handle_input_change';
 
 const rootReducer = combineReducers({
   channel_search: ChannelSearch,
--- src/reducers/reducer_channel_search.js
+++ src/reducers/reducer_channel_search.js
@@ -1,10 +1,13 @@
-import { CHANNEL_SEARCH } from '../actions/index';
+import { CHANNEL_SEARCH, CHANNEL_SEARCH_RESPONSE } from '../actions/index';
 
-export default function (state = {}, action) {
+export default function (state = null, action) {
 	switch(action.type) {
+		case CHANNEL_SEARCH_RESPONSE:
+			console.log(`Now in the "CHANNEL_SEARCH_RESPONSE" reducer.`);
+			return action.payload;
+
 		case CHANNEL_SEARCH:
 			console.log(`Now in the "CHANNEL_SEARCH" reducer.`);
-			console.log(action);
 			return state;
 	}
 
