0ad06716c0e545e9550f541285917c93e74906d8
Bernin Alejandro Uben <berninuben@icloud.com>
2018-05-28 23:10:12 -0400

    added 'active channel' styling for selected 'SearchListItem' && updated css to darkish theme

--- src/components/search_results_item.js
+++ src/components/search_results_item.js
@@ -16,26 +16,51 @@ class SearchResultsItem extends Component {
 	}
 
 	render() {
-		return (
-			<li className="search-results-item" key={this.props.key} onClick={this.setActiveClick}>
-				<div className="item-image-container">
-					<img src={this.props.data.logo} />
-				</div>
-				<div className="item-meta">
-					<p className="meta-name"> {this.props.data.display_name} </p>
-					<p className="meta-status"> {this.props.data.status.length < 65 ? this.props.data.status : this.props.data.status.substring(0,60) + "..."} </p>
-					<p className="meta-followers"> {this.props.data.followers} Followers </p>
-				</div>
-			</li>
-		);
+		if (this.props.active_channel) {
+			return (
+				<li className={ this.props.active_channel._id === this.props.data._id ? 'search-results-item active-channel' : 'search-results-item' } onClick={this.setActiveClick}>
+					<div className="item-image-container">
+						<img src={this.props.data.logo} />
+					</div>
+					<div className="item-meta">
+						<p className="meta-name"> {this.props.data.display_name} </p>
+						<p className="meta-status"> {this.props.data.status.length < 65 ? this.props.data.status : this.props.data.status.substring(0,60) + "..."} </p>
+						<p className="meta-followers"> {this.props.data.followers} Followers </p>
+					</div>
+				</li>
+			);
+		} else {
+			return (
+				<li className='search-results-item' onClick={this.setActiveClick}>
+					<div className="item-image-container">
+						<img src={this.props.data.logo} />
+					</div>
+					<div className="item-meta">
+						<p className="meta-name"> {this.props.data.display_name} </p>
+						<p className="meta-status"> {this.props.data.status.length < 65 ? this.props.data.status : this.props.data.status.substring(0,60) + "..."} </p>
+						<p className="meta-followers"> {this.props.data.followers} Followers </p>
+					</div>
+				</li>
+			);
+		}
 	}
 }
 
-function mapDispatchToProps(dispatch) {
+// && (this.props.active_channel._id === this.props.data._id)
+
+
+const mapStateToProps = (redux_state) => {
+	return {
+		active_channel: redux_state.active_channel,
+	};
+}
+
+const mapDispatchToProps = (dispatch) => {
 	return bindActionCreators({ set_active_channel: set_active_channel }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(SearchResultsItem);
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResultsItem);
 
 
 
